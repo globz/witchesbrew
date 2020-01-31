0104e2eb0870604928ff44955266a1576bff5273
globz <globz@kittybomber.com>
2020-01-31 15:52:48 -0500

    fixing source command, trying with interactive mode

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash -i
 #bashrc alias for witchesbrew/cauldron.sh
+
+echo "Installing witchesbrew...press ENTER to continue or CTRL+C to cancel"
 
 function witchesbrew () {
   source ~/witchesbrew/cauldron.sh
