b731c59ca80a5b7ff9396cb4e26b4461b58f4111
globz <globz@kittybomber.com>
2020-02-19 17:00:11 -0500

    Adeed brew-unixodbc (testing phase #1)

---
+++ brews/dexterity/brew-unixodbc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#iREP server - [unixODBC]
+#source : http://www.unixodbc.org/
+
+function brew_unixodbc() {
+
+  local ENV=$1
+  local DIR=$(locate -br "^witchesbrew$")
+  local REAGENTS="$DIR/brews/dexterity/reagents"
+
+  echo "Brewing unixODBC..."
+
+  cp REAGENTS/unixODBC-2.3.7.tar /home/$USER/
+  tar xvf /home/$USER/unixODBC-2.3.7.tar
+
+  sudo apt-get install build-essential
+
+  cd /home/$USER/unixODBC-2.3.7/
+  ./configure && make
+
+  if echo `odbcinst --version` | grep -q "unixODBC 2.3.7"
+  then
+    echo "unixODBC has been successfully installed for ENV : $ENV"
+  else
+    echo "unixODBC failed to install for ENV : $ENV"
+  fi
+
+}
---
+++ brews/intellect/brew-fail2ban.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#iREP server - [fail2ban]
+#source : https://github.com/fail2ban/fail2ban
