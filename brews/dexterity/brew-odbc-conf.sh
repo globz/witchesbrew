1801e9f7c0816e53437c7e3a973827d575b1f07b
globz <globz@kittybomber.com>
2020-02-20 17:24:08 -0500

    Added hard symlink to /etc/ for PHP

--- brews/dexterity/brew-odbc-conf.sh
+++ brews/dexterity/brew-odbc-conf.sh
@@ -12,14 +12,18 @@ function brew_odbc_conf() {
   local ODBC_INI=$REAGENTS/odbc-ini/iREP-$ENV
   local ODBC_INST=$REAGENTS/odbc-ini/odbcinst.ini
 
-  #odbcinst -i -s -f ODBC_CONF
   cp -R $ODBC_INI /home/$USER/odbc-ini/
   cp $ODBC_INST /home/$USER/odbc-ini/
 
   #ENV variables
   export ODBCSYSINI=/home/$USER/odbc-ini
   export ODBCINI=/home/$USER/odbc-ini/odbc.ini
-  #export LD_LIBRARY_PATH=/usr/lib64/:$LD_LIBRARY_PATH
+
+  #Create Hard symlink to /etc/
+  #PHP reads this location by default
+  #Edit mades in /home/$USER/odbc-ini will be reflected in /etc/
+  sudo ln /home/$USER/odbc-ini/odbc.ini /etc/
+  sudo ln /home/$USER/odbc-ini/odbcinst.ini /etc/
 
   echo "ODBC configuration files are now active, please edit and configure the user/password for both odbc.ini files located @"
   odbcinst -j
