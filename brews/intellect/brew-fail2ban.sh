4fcf543d2c090cc794fd391a529debec9837caef
globz <globz@kittybomber.com>
2020-02-26 14:30:26 -0500

    Added brew-fail2ban.sh

--- brews/intellect/brew-fail2ban.sh
+++ brews/intellect/brew-fail2ban.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
 #iREP server - [fail2ban]
+#wiki : https://github.com/fail2ban/fail2ban/wiki
 #source : https://github.com/fail2ban/fail2ban
+#setup : https://www.linode.com/docs/security/using-fail2ban-to-secure-your-server-a-tutorial/
+
+function brew_fail2ban() {
+
+  local ENV=$1
+  local DIR=$(locate -br "^witchesbrew$")
+  local REAGENTS="$DIR/brews/intellect/reagents"
+  local JAIL_LOCAL=$REAGENTS/fail2ban/iREP-$ENV/jail.local
+  local iREP_JAIL_CONF=$REAGENTS/fail2ban/jails/irep-auth.conf
+
+  echo "Brewing fail2ban..."
+
+  sudo apt install fail2ban
+
+  #Configuration change must be made to .local files
+  sudo touch /etc/fail2ban/fail2ban.local
+  sudo touch /etc/fail2ban/jail.local
+
+  #Custom iREP Jail
+  sudo cp $iREP_JAIL_CONF /etc/fail2ban/filter.d/
+
+  sudo cat $JAIL_LOCAL > /etc/fail2ban/jail.local
+
+  sudo fail2ban-client reload
+
+  echo "fail2ban is now properly configured for ENV : $ENV"
+
+}
--- brews/intellect/brew-iptables.sh
+++ brews/intellect/brew-iptables.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #iREP server - [iptables]
+#TODO : https://askubuntu.com/questions/1052919/iptables-reload-restart-on-ubuntu-18-04
 
 function brew_iptables() {
 
@@ -14,11 +15,13 @@ function brew_iptables() {
   sudo apt install iptables
 
   sudo iptables-restore < V4
-  sudo iptables-restore < V6
+  sudo ip6tables-restore < V6
 
   sudo apt install iptables-persistent
 
   sudo invoke-rc.d iptables-persistent save
+
+  sudo iptables -L && sudo ip6tables -L
 
   echo "iptables are now properly configured for ENV : $ENV"
 
