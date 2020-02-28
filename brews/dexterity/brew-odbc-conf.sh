7e97e2c054463cfb50df378f6e76ab55a6398895
globz <globz@kittybomber.com>
2020-02-28 16:23:55 -0500

    Added exports checks

--- brews/dexterity/brew-odbc-conf.sh
+++ brews/dexterity/brew-odbc-conf.sh
@@ -17,22 +17,29 @@ function brew_odbc_conf() {
   cp -R $ODBC_INI $ODBC_DIR
   cp $ODBC_INST $ODBC_DIR
 
-  #ENV variables - TODO Validate if already exist...
-  echo "export ODBCSYSINI=$ODBC_DIR" | sudo tee -a ~/.bashrc
-  echo "export ODBCSYSINI=$ODBC_DIR" | sudo tee -a ~/.bash_profile
-  echo "export ODBCINI=$ODBC_DIR/odbc.ini" | sudo tee -a ~/.bashrc
-  echo "export ODBCINI=$ODBC_DIR/odbc.ini" | sudo tee -a ~/.bash_profile
+  #ENV variables
+  if grep -Fq "ODBCSYSINI" ~/.bashrc || grep -Fq "ODBCSYSINI" ~/.bash_profile || grep -Fq "ODBCINI" ~/.bashrc || grep -Fq "ODBCINI" ~/.bash_profile
+  then
+    echo "export ODBCSYSINI & ODBCINI are already present in ~/.bashrc & ~/.bash_profile"
+    echo "Please remove those exports and try again..."
+  else
+    echo "export ODBCSYSINI=$ODBC_DIR" | sudo tee -a ~/.bashrc
+    echo "export ODBCSYSINI=$ODBC_DIR" | sudo tee -a ~/.bash_profile
+    echo "export ODBCINI=$ODBC_DIR/odbc.ini" | sudo tee -a ~/.bashrc
+    echo "export ODBCINI=$ODBC_DIR/odbc.ini" | sudo tee -a ~/.bash_profile
 
-  source ~/.bashrc
-  source ~/.bash_profile
+    source ~/.bashrc
+    source ~/.bash_profile
 
-  #Create Hard symlink to /etc/
-  #PHP reads this location by default
-  #Edit mades in /home/$USER/odbc-ini will be reflected in /etc/
-  sudo ln $ODBC_DIR/odbc.ini /etc/
-  sudo ln $ODBC_DIR/odbcinst.ini /etc/
+    #Create Hard symlink to /etc/
+    #PHP reads this location by default
+    #Edit mades in /home/$USER/odbc-ini will be reflected in /etc/
+    sudo ln $ODBC_DIR/odbc.ini /etc/
+    sudo ln $ODBC_DIR/odbcinst.ini /etc/
 
-  echo "ODBC configuration files are now active, please edit and configure the user/password for both odbc.ini files located @"
-  odbcinst -j
+    echo "ODBC configuration files are now active, please edit and configure the user/password for both odbc.ini files located @"
+    odbcinst -j
+
+  fi
 
 }
