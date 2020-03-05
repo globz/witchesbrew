1540aa0a5e1ceab675367a4a5e9cac771f3f03c1
globz <globz@kittybomber.com>
2020-03-05 17:20:51 -0500

    Git will no longer track file permission

--- brews/strength/brew-toolshed.sh
+++ brews/strength/brew-toolshed.sh
@@ -4,12 +4,17 @@
 function brew_toolshed() {
 
   local ENV=$1
+  local GIT_DIR="/home/$USER/git-irep-toolshed"
+  local GIT_REPO="/var/www/toolshed"
 
   echo "Brewing iREP-toolshed..."
 
   #Clone iREP-toolshed from master branch
   #git-dir is located @ /home/$USER/git-irep-toolshed
-  git clone --separate-git-dir=/home/$USER/git-irep-toolshed https://github.com/globz/iREP-toolshed.git /var/www/toolshed
+  git clone --separate-git-dir=$GIT_DIR https://github.com/globz/iREP-toolshed.git $GIT_REPO
+
+  #Stop tracking file permission
+  (cd $GIT_DIR && git config --local core.fileMode false)
 
   echo "iREP-toolshed has been successfully cloned @ /var/www/toolshed for ENV : $ENV"
 
