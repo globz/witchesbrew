bed235f2d080f46dac967cdb074e10420e84d8ac
globz <globz@kittybomber.com>
2023-10-22 00:37:19 -0400

    Now wrapped inside a function call

--- install.sh
+++ install.sh
@@ -2,11 +2,27 @@
 
 set -e
 
+if [ $(readlink /proc/$$/exe)  != "/usr/bin/bash" ]; then echo "Please run this script with bash"; exit; fi
+
+witchesbrew_do_install() {
 echo "Installing witchesbrew..."
+
+
+if [ -f "$HOME/bin/witchesbrew" ];
+then
+    echo "Aborting. You should first uninstall witchesbrew."
+    return 1
+fi
+
+if [ -L "$HOME/bin/witchesbrew_wd" ];
+then
+    echo "Aborting. You should first uninstall witchesbrew."
+    return 1
+fi
 
 # gawk requirement
 _gawk=$( command -v gawk )
-if [[ -z "${_gawk}" ]];
+if [ -z "${_gawk}" ];
 then
     echo -e "\033[31mError :\e[0m Aborting installation... [host-requirement: gawk is not installed]"
     exit 1
@@ -19,10 +35,19 @@ then
 if [ -d "$HOME/bin" ]; then
     export PATH="$HOME/bin:$PATH"
 fi
-. "$HOME/bin/witchesbrew_wd/witchesbrew_completion.sh"
 EOF
 else
     echo "$HOME/bin already exported to PATH"
+fi
+
+# bash_completion
+if ! grep -Fq '\. "$HOME/bin/witchesbrew_wd/witchesbrew_completion.sh"' ~/.profile
+then
+    cat<<"EOF" >> ~/.profile
+\. "$HOME/bin/witchesbrew_wd/witchesbrew_completion.sh"
+EOF
+else
+    echo "bash_completion for witchesbrew is already present"
 fi
 
 # Create $HOME/bin if not available
@@ -42,7 +67,7 @@ function repl() {
 
 typeset -f > $HOME/bin/witchesbrew && echo 'repl "$@"' >> $HOME/bin/witchesbrew
 chmod u+x $HOME/bin/witchesbrew
-ln -sf "$(pwd)" $HOME/bin/witchesbrew_wd 
+ln -sf "$(pwd)" $HOME/bin/witchesbrew_wd
 
 cat<<"EOF"
                              (
@@ -69,3 +94,8 @@ https://github.com/globz/witchesbrew
 
 EOF
 echo "witchesbrew is now installed."
+echo -e "\nClose and reopen your terminal to start using witchesbrew"
+
+}
+
+witchesbrew_do_install
